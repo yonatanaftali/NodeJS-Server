b0a77c798e4391c978383fa218ea6ede8581f126
naftayo <naftayo@post.bgu.ac.il>
2021-07-08 00:18:25 +0300

    Add an item

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const fs = require('fs') // Native nodejs library
 const app = express() // Init express app
+// These two configures the post format for express
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // A get request to the root directory
 // Whenever we get an http request to the '/'path, the function handling this request will get a request and a response that we have to capure
@@ -9,6 +12,7 @@ app.get('/', (request, response) => {
     // When we hit the root dir of we'll send a response 'Hello, world!'
 })
 
+// Get the todo list, with an option to filter on pending only
 app.get('/todos', (request, response) => {
 
     const showOnlyPending = request.query.showPending
@@ -20,10 +24,11 @@ app.get('/todos', (request, response) => {
         const todos = JSON.parse(data)
         if (showOnlyPending !== "1")
             return response.json({ todos: todos })
-        return response.json({ todos: todos.filter(item => item.complete === false) })
+        return response.json({ todos: todos.filter(item => item.completed === false) })
     })
 })
 
+// Complete an item, if exists
 app.put('/todos/:id/complete', (request, response) => {
 
     const findToDoById = (todos, id) => {
@@ -42,9 +47,28 @@ app.put('/todos/:id/complete', (request, response) => {
         const todoIndex = findToDoById(todos, id)
         if (todoIndex === -1)
             return response.status(404).send("Error: ID not found")
-        todos[todoIndex].complete = true
+        todos[todoIndex].completed = true
         fs.writeFile('./store/todos.json', JSON.stringify(todos), () =>
-            response.status(200).json({ 'status': 'Completed', 'id': id }))
+            response.status(200).json({ 'Action': 'Completing a todo itme', 'status': 'Completed', 'id': id }))
+    })
+})
+
+// Add an item
+app.post('/todo', (request, response) => {
+    if (!request.body.name)
+        return response.status(400).send("Missing name")
+    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+        if (err)
+            return response.status(500).send("Error: Can't read todo file")
+        let todos = JSON.parse(data)
+        const nextFreeID = Math.max.apply(Math, todos.map(item => item.id)) + 1
+        todos.push({
+            id: nextFreeID,
+            name: request.body.name,
+            completed: false
+        })
+        fs.writeFile('./store/todos.json', JSON.stringify(todos), () =>
+            response.status(200).json({ 'Action': 'Adding a todo item', 'status': 'success', 'name': request.body.name }))
     })
 })
 
