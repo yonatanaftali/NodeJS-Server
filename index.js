d180dc40b59c906317a5daa3f68f814a673f7586
naftayo <naftayo@post.bgu.ac.il>
2021-07-07 23:41:23 +0300

    wip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs') // Native nodejs library
 const app = express() // Init express app
 
 // A get request to the root directory
@@ -6,6 +7,39 @@ const app = express() // Init express app
 app.get('/', (request, response) => {
     return response.send("Hello, world!")
     // When we hit the root dir of we'll send a response 'Hello, world!'
+})
+
+app.get('/todos', (request, response) => {
+    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+        if (err) // In case readfile failes reading the file for any reason
+            return response.status(500).send("Sorry, something went wrong")
+        // Otherwise, we need to load the data (that is passed to us as a string) to JSON, so we can manipulate it
+        const todos = JSON.parse(data)
+        return response.json({ todos: todos })
+    })
+})
+
+app.put('/todos/:id/complete', (request, response) => {
+
+    const findToDoById = (todos, id) => {
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].id === parseInt(id))
+                return i
+        }
+        return -1 // in case we didn't find the item
+    }
+
+    fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+        if (err)
+            return response.status(500).send("Error: Can't read todo file")
+        const todos = JSON.parse(data)
+        const id = request.params.id
+        const todoIndex = findToDoById(todos, id)
+        if (todoIndex === -1)
+            return response.status(404).send("Error: ID not found")
+        else
+            return response.status(200).send(todos[todoIndex])
+    })
 })
 
 // Define port for inbound traffic
